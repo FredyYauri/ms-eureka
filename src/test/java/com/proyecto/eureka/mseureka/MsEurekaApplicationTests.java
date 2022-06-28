4ac047ccc0563ff55627a2ddd1b6b62bcb5227c5
fyauriba <fyauriba@everis.com>
2022-06-27 23:33:01 -0500

    server eureka

---
+++ src/test/java/com/proyecto/eureka/mseureka/MsEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.eureka.mseureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
